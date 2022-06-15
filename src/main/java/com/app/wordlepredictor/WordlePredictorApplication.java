f1a66cdedbb1dfaa735eb9d47ae410a9e83fc0c1
Hayden <haydenbryant.student@careerdevs.com>
2022-06-14 20:41:32 -0400

    initial commit

---
+++ src/main/java/com/app/wordlepredictor/WordlePredictorApplication.java
@@ -0,0 +1,13 @@
+package com.app.wordlepredictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordlePredictorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordlePredictorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/wordlepredictor/WordlePredictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.wordlepredictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordlePredictorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
